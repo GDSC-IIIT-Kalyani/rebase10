b18908565c685a8d943011897f3d66e96adb13ff
Soaham Sur <soaham1028messi@gmail.com>
2024-11-05 23:47:30 +0530

    faqs

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css';
 import Navbar from './Navbar';
 import Onboarding from './Onboarding';
 import About from './About';
-
+import Faqs from './Faqs';
 function App() {
   return (
     <div>
     <Navbar/>
     <Onboarding/>
     <About/>
+    <Faqs/>
     </div>
   );
 }
